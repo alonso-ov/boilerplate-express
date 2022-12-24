082bf839c28265a8a72d330dbfc8330bb250fef9
Alonso Ortiz <alonsoortiz73@gmail.com>
2022-12-23 16:19:49 -0800

    forgot to add - to the console.log

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ let app = express();
 app.use("/public", express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
-    console.log(req.method + " " + req.path + " " + req.ip)
+    console.log(req.method + " " + req.path + " -" + req.ip)
     next()
 })
 
