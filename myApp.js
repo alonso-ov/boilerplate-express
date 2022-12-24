57acbd2234cb8004f3a46bc60ba09e09bc0f6e15
Alonso Ortiz <alonsoortiz73@gmail.com>
2022-12-23 17:26:11 -0800

    changed function position

--- myApp.js
+++ myApp.js
@@ -4,9 +4,10 @@ const bodyParser = require('body-parse')
 const express = require('express');
 const app = express();
 
-app.use("/public", express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use("/public", express.static(__dirname + '/public'))
 
 app.use("/", (req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip)
