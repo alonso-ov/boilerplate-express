bf7073034b8b47b6697f065103f812ae72a348f1
Alonso Ortiz <alonsoortiz73@gmail.com>
2022-12-23 16:14:36 -0800

    added middleware for root

--- myApp.js
+++ myApp.js
@@ -3,7 +3,12 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
-app.use('/public', express.static(__dirname + '/public'))
+app.use("/public", express.static(__dirname + '/public'))
+
+app.use("/", (req, res, next) => {
+    console.log(req.method + " " + req.path + " " + req.ip)
+    next()
+})
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
